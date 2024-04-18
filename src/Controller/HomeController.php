9e8dcbab970f187065024388e23a0fe9aa98c33e
Dilshad <annonym208@gmail.com>
2024-04-18 17:09:51 +0200

    Meldingen zijn te zijn op de map Markers zijn clickable als je op de marker gaat en dan klikt op de id brengt die jou naar de meldingenen details page waar je de foto kunt zien de tekts en de categorie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,18 +4,31 @@
 
 namespace App\Controller;
 
+use App\Repository\MeldingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+private $meldingRepository;
+
+public function __construct(MeldingRepository $meldingRepository)
+{
+$this->meldingRepository = $meldingRepository;
+}
+
 /**
 * @Route("/", name="home")
 */
 public function index(): Response
 {
-// Render de Twig-sjabloon voor de homepagina
-return $this->render('security/home.html.twig');
+// Haal de meldingen op uit de repository
+$meldingen = $this->meldingRepository->findAll();
+
+// Render de Twig-sjabloon voor de homepagina en geef de meldingen door als variabele
+return $this->render('security/home.html.twig', [
+'meldingen' => $meldingen,
+]);
 }
 }
